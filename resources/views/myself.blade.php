6bdfa2147ee4f78c3f0982013f67a9fa6cbab3dd
akaz-cyber <ajrisonic62@gmail.com>
2021-02-06 08:35:58 +0700

    first commit

---
+++ resources/views/myself.blade.php
@@ -0,0 +1,16 @@
+<h1><center>MYSELF</center></h1>
+ Id eaque necessitatibus reiciendis assumenda rem nulla maiores vel accusantium, aspernatur expedita neque ipsum? Ducimus accusantium iure, distinctio provident dicta rerum dolores.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis officiis earum eveniet dolorem, quibusdam laudantium impedit aliquid dolorum pariatur quasi perferendis? 
+  Ullam sed, tempore possimus quas officiis quisquam laboriosam error!
+ <br>
+ </br>
+ <h2><center>PROPIL</center></h2>  <center>
+ <img src="{{URL:: to('/ajri/ajri.jpeg')}}" height="500px"align="center">
+</center
+ 
+ <br>
+ </br>
+ <center><b>Nama : ajri muhammad shiddik</b></center>
+ <br> 
+ </br>
+ <center><b>kelas : XI RPL </b></center>
